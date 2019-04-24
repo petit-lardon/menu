f6ae14d30f70619eb73bb0ed4de4f4af2e51e841
Julien LAVIE <jlavie@leadformance.com>
2019-04-25 00:58:13 +0200

    feat(binding): practice multi binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
