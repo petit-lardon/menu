b7ecdc30e431d52bab65d48fedf522a64077ce03
Julien LAVIE <jlavie@leadformance.com>
2019-04-25 00:00:54 +0200

    feat(binding): pass data into event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server created';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -20,5 +21,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
